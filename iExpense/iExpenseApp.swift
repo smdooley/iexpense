501c4320ac9b5778b2f576e366611c821f81799e
Sean Dooley <s.dooley@ctidigital.com>
2024-11-12 10:45:28 +0000

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Sean Dooley on 12/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
