faf190c9d71af2e7fe903c62296aea2f4dd05311
Sean Dooley <s.dooley@ctidigital.com>
2024-11-12 13:47:28 +0000

    sharing an observed object with a new view

---
+++ iExpense/AddView.swift
@@ -0,0 +1,41 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Sean Dooley on 12/11/2024.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var expenses: Expenses
+    
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    let types = ["Business", "Personal"]
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", value: $amount, format: .currency(code: "USD")
+                )
+                .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+        }
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var expenses = Expenses()
+    @State private var showingAddExpense = false
     
     var body: some View {
         NavigationStack {
@@ -21,10 +22,14 @@ struct ContentView: View {
             .navigationTitle("iExpense")
             .toolbar {
                 Button("Add expense", systemImage: "plus") {
-                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
-                    expenses.items.append(expense)
+                    //let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    //expenses.items.append(expense)
+                    showingAddExpense = true
                 }
             }
+        }
+        .sheet(isPresented: $showingAddExpense) {
+            AddView(expenses: expenses)
         }
     }
     
