079367b111fb29e6954c86776567be2c1ba57081
Sean Dooley <s.dooley@ctidigital.com>
2024-11-12 13:56:06 +0000

    making changes permanent with userdefaults

--- iExpense/Expenses.swift
+++ iExpense/Expenses.swift
@@ -9,5 +9,23 @@ import Foundation
 
 @Observable
 class Expenses {
-    var items = [ExpenseItem]()
+    //var items = [ExpenseItem]()
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "items")
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        
+        items = []
+    }
 }
