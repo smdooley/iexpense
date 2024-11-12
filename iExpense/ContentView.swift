1f6ccb55938a67f6e82a679fd06413a9b5f18e81
Sean Dooley <s.dooley@ctidigital.com>
2024-11-12 14:26:12 +0000

    Use the user’s preferred currency

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View {
                             Text(item.type)
                         }
                         Spacer()
-                        Text(item.amount, format: .currency(code: "USD"))
+                        Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                     }
                     
                 }
