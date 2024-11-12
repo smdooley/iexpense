542825113f5965b5fa8d5e7d49e340dc1047f412
Sean Dooley <s.dooley@ctidigital.com>
2024-11-12 13:59:17 +0000

    final polish

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct AddView: View {
     var expenses: Expenses
     
+    @Environment(\.dismiss) var dismiss
+    
     @State private var name = ""
     @State private var type = "Personal"
     @State private var amount = 0.0
@@ -36,6 +38,7 @@ struct AddView: View {
                 Button("Save") {
                     let item = ExpenseItem(name: name, type: type, amount: amount)
                     expenses.items.append(item)
+                    dismiss()
                 }
             }
         }
